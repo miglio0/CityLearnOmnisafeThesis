83457a6d43d3fbd69e9abed513585f54280cce7f
Lorenzo Bonanni <lorenzopuntobonanni@gmail.com>
2026-01-21 09:54:36 +0100

    fix placeholder cost function

--- CityLearn/citylearn/cmdp_cost_functions.py
+++ CityLearn/citylearn/cmdp_cost_functions.py
@@ -3,4 +3,4 @@ import torch
 
 def placeholder_cost_fn(obs: torch.Tensor) -> torch.Tensor:
     # Placeholder cost function that returns zero cost
-    return torch.tensor([0.0], dtype=torch.float32)
+    return torch.tensor(0.0, dtype=torch.float32)
